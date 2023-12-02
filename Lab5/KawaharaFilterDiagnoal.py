ab7ec8858cc75e742b8d54e5e190fae72dc47d55
wallace611 <wallacef930814@gmail.com>
2023-12-02 11:15:21 +0800

    update code

--- Lab5/KawaharaFilterDiagnoal.py
+++ Lab5/KawaharaFilterDiagnoal.py
@@ -12,21 +12,53 @@ def Kuwahara(original, winsize):
         raise Exception ("Invalid winsize %s: winsize must follow formula: w = 4*n+1." %winsize)
 
     #Build subwindows
-    arr = []
-    for i in range(winsize):
-        count = winsize - i*2
-        if count > 0:
-            arr.append([])
-            for _ in range(i):
-                arr[i].append(0)
-            for _ in range(count):
-                arr[i].append(1)
-            for _ in range(i):
-                arr[i].append(0)
-        else:
-            arr.append([0 for _ in range(winsize)])
+    tmpAvgKerRow = np.hstack((np.ones((1,(winsize-1)//2+1)),np.zeros((1,(winsize-1)//2))))
+    tmpPadder = np.zeros((1,winsize))
+    tmpavgker = np.tile(tmpAvgKerRow, ((winsize-1)//2+1,1))
+    tmpavgker = np.vstack((tmpavgker, np.tile(tmpPadder, ((winsize-1)//2,1))))
+    tmpavgker = tmpavgker/np.sum(tmpavgker)
 
-    tmpavgker = np.array(arr)
+    # tmpavgker is a 'north-west' subwindow (marked as 'a' above)
+    # we build a vector of convolution kernels for computing average and
+    # variance
+    avgker = np.empty((4,winsize,winsize)) # make an empty vector of arrays
+    avgker[0] = tmpavgker			# North-west (a)
+    avgker[1] = np.fliplr(tmpavgker)	# North-east (b)
+    avgker[2] = np.flipud(tmpavgker)	# South-west (c)
+    avgker[3] = np.fliplr(avgker[2])	# South-east (d)
+    # Create a pixel-by-pixel square of the image
+    squaredImg = image**2 
+# preallocate these arrays to make it apparently %15 faster
+    avgs = np.zeros([4, image.shape[0],image.shape[1]])
+    stddevs = avgs.copy()
+
+    # Calculation of averages and variances on subwindows
+    for k in range(4):
+        # mean on subwindow
+        avgs[k] = convolve2d(image, avgker[k],mode='same') 	    
+        # mean of squares on subwindow
+        stddevs[k] = convolve2d(squaredImg, avgker[k],mode='same')
+        # variance on subwindow
+        stddevs[k] = stddevs[k]-avgs[k]**2
+    # Choice of index with minimum variance
+    indices = np.argmin(stddevs,0) # returns index of subwindow with smallest variance
+
+    # Building the filtered image (with nested for loops)
+    filtered = np.zeros(original.shape)
+    for row in range(original.shape[0]):
+        for col in range(original.shape[1]):
+            filtered[row,col] = avgs[indices[row,col], row,col]
+
+    #filtered=filtered.astype(np.uint8)
+    return filtered.astype(np.uint8)
+
+def KuwaharaDiag(original, winsize):
+    image = original.astype(np.float64)
+    # make sure window size is correct
+    if winsize %4 != 1:
+        raise Exception ("Invalid winsize %s: winsize must follow formula: w = 4*n+1." %winsize)
+
+    tmpavgker = np.triu(np.full((winsize, winsize), 1) - np.rot90(np.tri(winsize, winsize, -1)))
     tmpavgker = tmpavgker / np.sum(tmpavgker)
     # tmpavgker is a 'north-west' subwindow (marked as 'a' above)
     # we build a vector of convolution kernels for computing average and
@@ -66,14 +98,17 @@ def Kuwahara(original, winsize):
 c = io.imread('Digital-Image-Processing\\Lab5\\cameraman.tif')
 
 cK=Kuwahara(c,9)
+cKD=KuwaharaDiag(c, 9)
 
 
 fig = plt.figure()
 plt.gray()  # show the filtered result in grayscale
-ax1 = fig.add_subplot(121)  # left side
-ax2 = fig.add_subplot(122)  # right side
+ax1 = fig.add_subplot(131)  # left side
+ax2 = fig.add_subplot(132)  # right side
+ax3 = fig.add_subplot(133)
 
 ax1.imshow(c/255,vmax=1.0,vmin=0.0)
 ax2.imshow(cK/255,vmax=1.0,vmin=0.0)
+ax3.imshow(cKD/255, vmax=1.0, vmin=0.0)
 
 plt.show()
