68e2274a015d6177f5f3bd0c09d01555ac87de67
wallace611 <wallacef930814@gmail.com>
2023-12-25 20:30:32 +0800

    %date%

--- Exam/5.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from skimage import io
-
-img = io.imread('cameraman.tif')
-
--- Exam/7.py
+++ Exam/7.py
@@ -1,3 +1,6 @@
 import numpy as np
 from skimage import io
 from scipy import ndimage as ndi
+
+img = io.imread('cameraman.tif')
+
